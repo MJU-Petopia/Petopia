e0190688a4f880d11fe3d005c085d3fdb315ed25
sunms0710 <sunms0710@naver.com>
2024-05-16 00:05:28 +0900

    username unique 제외

--- src/main/java/com/petopia/domain/user/User.java
+++ src/main/java/com/petopia/domain/user/User.java
@@ -24,7 +24,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(length = 100, unique = true)
+    @Column(length = 100)
     private String username;
 
     @Column(nullable = false)
