fa4c72a3d2af8a8a0670138881d6bc7bb370ceb9
sunms0710 <sunms0710@naver.com>
2024-05-16 17:45:04 +0900

    cors 설정

--- src/main/java/com/petopia/config/WebMvcConfig.java
+++ src/main/java/com/petopia/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.petopia.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -22,5 +23,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .setCachePeriod(60*10*6)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PATCH", "PUT", "DELETE");
     }
 }
