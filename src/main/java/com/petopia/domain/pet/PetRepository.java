7f94b30d4e4f126d6ceb93d661a0a12493806060
sunms0710 <sunms0710@naver.com>
2024-05-25 20:36:07 +0900

    로직 수정

--- src/main/java/com/petopia/domain/pet/PetRepository.java
+++ src/main/java/com/petopia/domain/pet/PetRepository.java
@@ -13,6 +13,6 @@ public interface PetRepository extends JpaRepository<Pet, Integer> {
     Page<Pet> mList(Pageable pageable, @Param("userId") int userId);
 
     @Modifying
-    @Query(value = "DELETE FROM Post WHERE id=:petId AND userId =:userId", nativeQuery = true)
+    @Query(value = "DELETE FROM Pet WHERE id=:petId AND userId =:userId", nativeQuery = true)
     void mDelete(@Param("petId")int petId, @Param("userId") int userId);
 }
