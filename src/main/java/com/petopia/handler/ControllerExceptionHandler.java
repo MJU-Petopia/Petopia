50f904fd3435d4ffb0456b1d6bc923991adc62e9
sunms0710 <sunms0710@naver.com>
2024-05-26 01:03:46 +0900

    수정

--- src/main/java/com/petopia/handler/ControllerExceptionHandler.java
+++ src/main/java/com/petopia/handler/ControllerExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.petopia.handler;
 
 import com.petopia.handler.ex.CustomApiException;
-import com.petopia.handler.ex.CustomException;
 import com.petopia.handler.ex.CustomValidationApiException;
-import com.petopia.handler.ex.CustomValidationException;
 import com.petopia.response.CMRespDto;
-import com.petopia.util.Script;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,20 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice
 @RestController
 public class ControllerExceptionHandler {
-
-    @ExceptionHandler(CustomValidationException.class)
-    public String validationException(CustomValidationException e) {
-        if(e.getErrorMap() == null){
-            return Script.back(e.getMessage());
-        }else {
-            return Script.back(e.getErrorMap().toString());
-        }
-    }
-
-    @ExceptionHandler(CustomException.class)
-    public String exception(CustomException e) {
-        return Script.back(e.getMessage());
-    }
 
     @ExceptionHandler(CustomValidationApiException.class)
     public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationApiException e) {
