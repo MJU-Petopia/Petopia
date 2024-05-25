24cdb27b727a8daa64a25907a0b9439bfd1da783
sunms0710 <sunms0710@naver.com>
2024-05-26 00:48:48 +0900

    User 수정

--- src/main/java/com/petopia/request/user/UserUpdateDto.java
+++ src/main/java/com/petopia/request/user/UserUpdateDto.java
@@ -3,14 +3,11 @@ package com.petopia.request.user;
 import com.petopia.domain.user.User;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserUpdateDto {
 
-    @NotBlank
     private String name;
-    @NotBlank
     private String password;
     private String phone;
     private String gender;
